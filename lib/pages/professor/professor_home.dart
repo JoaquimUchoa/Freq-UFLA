def64a8dc9b49f5954d3288ff7a8feb96b6c86fe
vspinellig <viinicius.sg@hotmail.com>
2020-08-22 20:13:13 -0300

    listando disciplinas do professor.

--- lib/pages/professor/professor_home.dart
+++ lib/pages/professor/professor_home.dart
@@ -1,9 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-
 class ProfessorHome extends StatefulWidget {
-  ProfessorHome({Key key, this.userId, this.logoutCallback})
-      : super(key: key);
+  ProfessorHome({Key key, this.userId, this.logoutCallback}) : super(key: key);
 
   final VoidCallback logoutCallback;
   final String userId;
@@ -16,16 +15,46 @@ class _ProfessorHomeState extends State<ProfessorHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("ProfessorHome"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.exit_to_app),
-              onPressed: widget.logoutCallback,
-            ),
-          ],
-        ),
-        body: Text("ProfessorHome")
+      appBar: AppBar(
+        title: Text('Professor'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: widget.logoutCallback,
+          ),
+        ],
+      ),
+      body: StreamBuilder(
+          stream: Firestore.instance
+              .collection('periodos')
+              .document('2020-1')
+              .collection('disciplinas')
+              .where('professorId', isEqualTo: widget.userId)
+              .snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> disciplina) {
+            if (disciplina.hasError) {
+              return Center(child: Text('Error: ${disciplina.error}'));
+            }
+
+            if (disciplina.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            if (disciplina.data.documents.length == 0) {
+              return Center(
+                  child:
+                      Text('Você não está cadastrado em nenhuma disciplina'));
+            }
+
+            return ListView.builder(
+                itemCount: disciplina.data.documents.length,
+                itemBuilder: (BuildContext context, int i) {
+                  return ListTile(
+                      title: Text(
+                          '${disciplina.data.documents[i].documentID} - ${disciplina.data.documents[i].data['nome']}'));
+                });
+          }),
     );
   }
 }
