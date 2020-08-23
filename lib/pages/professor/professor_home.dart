4841dffe830e8817aa32e25dc6a00e3aaaeeef5e
vspinellig <viinicius.sg@hotmail.com>
2020-08-23 01:08:41 -0300

    colocando getDisciplinas no initState

--- lib/pages/professor/professor_home.dart
+++ lib/pages/professor/professor_home.dart
@@ -12,49 +12,66 @@ class ProfessorHome extends StatefulWidget {
 }
 
 class _ProfessorHomeState extends State<ProfessorHome> {
+  var _disciplinas;
+
+  @override
+  void initState() {
+    setState(() {
+      _disciplinas = Firestore.instance
+          .collection('periodos')
+          .document('2020-1')
+          .collection('disciplinas')
+          .where('professorId', isEqualTo: widget.userId)
+          .snapshots();
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Professor'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.exit_to_app),
-            onPressed: widget.logoutCallback,
-          ),
-        ],
-      ),
-      body: StreamBuilder(
-          stream: Firestore.instance
-              .collection('periodos')
-              .document('2020-1')
-              .collection('disciplinas')
-              .where('professorId', isEqualTo: widget.userId)
-              .snapshots(),
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> disciplina) {
-            if (disciplina.hasError) {
-              return Center(child: Text('Error: ${disciplina.error}'));
-            }
+        appBar: AppBar(
+          title: Text('Disciplinas'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.exit_to_app),
+              onPressed: widget.logoutCallback,
+            ),
+          ],
+        ),
+        body: StreamBuilder(
+            stream: _disciplinas,
+            builder: (BuildContext context,
+                AsyncSnapshot<QuerySnapshot> disciplina) {
+              if (disciplina.hasError) {
+                return Center(child: Text('Error: ${disciplina.error}'));
+              }
 
-            if (disciplina.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            }
+              if (disciplina.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              }
 
-            if (disciplina.data.documents.length == 0) {
-              return Center(
-                  child:
-                      Text('Você não está cadastrado em nenhuma disciplina'));
-            }
+              if (disciplina.data.documents.length == 0) {
+                return Center(
+                    child:
+                        Text('Você não está cadastrado em nenhuma disciplina'));
+              }
 
-            return ListView.builder(
-                itemCount: disciplina.data.documents.length,
-                itemBuilder: (BuildContext context, int i) {
-                  return ListTile(
+              return ListView.builder(
+                  itemCount: disciplina.data.documents.length,
+                  itemBuilder: (BuildContext context, int i) {
+                    return ListTile(
                       title: Text(
-                          '${disciplina.data.documents[i].documentID} - ${disciplina.data.documents[i].data['nome']}'));
-                });
-          }),
-    );
+                          '${disciplina.data.documents[i].documentID} - ${disciplina.data.documents[i].data['nome']}'),
+                      onTap: () => {
+                        Navigator.of(context)
+                            .pushNamed('/professor/disciplina', arguments: [
+                          disciplina.data.documents[i].documentID,
+                          disciplina.data.documents[i].data['nome']
+                        ])
+                      },
+                    );
+                  });
+            }));
   }
 }
