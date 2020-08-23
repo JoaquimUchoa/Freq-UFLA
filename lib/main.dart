9dc5d2d5b7167f8ae584e8e18f5c27d508463df9
vspinellig <viinicius.sg@hotmail.com>
2020-08-23 01:10:26 -0300

    adicionando a rota da pagina da disciplina

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:freq_ufla/pages/professor/professor_disciplina.dart';
 import 'package:freq_ufla/services/authentication.dart';
 import 'package:freq_ufla/pages/root.dart';
+//import 'package:freq_ufla/pages/professor/professor_disciplina.dart';
 
 void main() {
   runApp(new MyApp());
@@ -15,6 +17,10 @@ class MyApp extends StatelessWidget {
         theme: new ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: new RootPage(auth: new Auth()));
+        home: new RootPage(auth: new Auth()),
+        routes: <String, WidgetBuilder>{
+          '/professor/disciplina': (BuildContext context) =>
+              ProfessorDisciplina(),
+        });
   }
 }
